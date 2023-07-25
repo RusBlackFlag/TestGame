71fc09d87a106506f910e2eb6e6e1232e22843db
Alex Smith <alex.one.com@gmail.com>
2023-07-25 23:08:05 +0700

    Character Status Part 25

--- src/main/EventHandler.java
+++ src/main/EventHandler.java
@@ -1,7 +1,5 @@
 package main;
 
-import java.awt.*;
-
 public class EventHandler {
 
     GamePanel gp;
@@ -81,6 +79,7 @@ public class EventHandler {
     public void damagePit(int col, int row, int gameState) {
 
         gp.gameState = gameState;
+        gp.playSE(6);
         gp.ui.currentDialogue = "You fall into a pit!";
         gp.player.life -= 1;
         //eventRect[col][row].eventDone = true;
@@ -91,6 +90,8 @@ public class EventHandler {
 
         if(gp.keyH.enterPressed == true) {
             gp.gameState = gameState;
+            gp.player.attackCanceled = true;
+            gp.playSE(2);
             gp.ui.currentDialogue = "You drink the water.\nYour life has been recovered.";
             gp.player.life = gp.player.maxLife;
         }
