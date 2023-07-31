751d781fe3674815e73cad4a315686026543ffbe
Alex Smith <alex.one.com@gmail.com>
2023-07-31 23:58:37 +0700

    Scrolling Message, Leveling Up, Damage Calculation - Part 26

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -2,10 +2,6 @@ package main;
 
 import entity.NPC_OldMan;
 import monster.MON_GreenSlime;
-import object.OBJ_Boots;
-import object.OBJ_Chest;
-import object.OBJ_Door;
-import object.OBJ_Key;
 
 public class AssetSetter {
 
@@ -25,26 +21,29 @@ public class AssetSetter {
         gp.npc[0].worldX = gp.tileSize*21;
         gp.npc[0].worldY = gp.tileSize*21;
 
-//        gp.npc[0] = new NPC_OldMan(gp);
-//        gp.npc[0].worldX = gp.tileSize*9;
-//        gp.npc[0].worldY = gp.tileSize*10;
     }
 
     public void setMonster() {
-        gp.monster[0] = new MON_GreenSlime(gp);
-        gp.monster[0].worldX = gp.tileSize*23;
-        gp.monster[0].worldY = gp.tileSize*36;
-
-        gp.monster[1] = new MON_GreenSlime(gp);
-        gp.monster[1].worldX = gp.tileSize*23;
-        gp.monster[1].worldY = gp.tileSize*37;
-
-//        gp.monster[0] = new MON_GreenSlime(gp);
-//        gp.monster[0].worldX = gp.tileSize*11;
-//        gp.monster[0].worldY = gp.tileSize*10;
-//
-//        gp.monster[1] = new MON_GreenSlime(gp);
-//        gp.monster[1].worldX = gp.tileSize*11;
-//        gp.monster[1].worldY = gp.tileSize*11;
+        int i = 0;
+        gp.monster[i] = new MON_GreenSlime(gp);
+        gp.monster[i].worldX = gp.tileSize*21;
+        gp.monster[i].worldY = gp.tileSize*38;
+        i++;
+        gp.monster[i] = new MON_GreenSlime(gp);
+        gp.monster[i].worldX = gp.tileSize*23;
+        gp.monster[i].worldY = gp.tileSize*42;
+        i++;
+        gp.monster[i] = new MON_GreenSlime(gp);
+        gp.monster[i].worldX = gp.tileSize*24;
+        gp.monster[i].worldY = gp.tileSize*37;
+        i++;
+        gp.monster[i] = new MON_GreenSlime(gp);
+        gp.monster[i].worldX = gp.tileSize*34;
+        gp.monster[i].worldY = gp.tileSize*42;
+        i++;
+        gp.monster[i] = new MON_GreenSlime(gp);
+        gp.monster[i].worldX = gp.tileSize*38;
+        gp.monster[i].worldY = gp.tileSize*42;
+        i++;
     }
 }
